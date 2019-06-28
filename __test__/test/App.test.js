309424ea04c4caa7e76ee7e5909c0f715f1f089b
DrZin <61010707@kmitl.ac.th>
2019-06-28 21:42:20 +0700

    update test APP

--- __test__/test/App.test.js
+++ __test__/test/App.test.js
@@ -3,9 +3,16 @@ import { shallow } from "enzyme";
 import App from "../../src/components/App.js";
 
 describe("App", () => {
+  const component = shallow(<App />);
   it("should render correctly with no props", () => {
-    const component = shallow(<App />);
-
     expect(component).toMatchSnapshot();
+  });
+
+  it("should contain App todoapp", () => {
+    expect(
+      shallow(<App />)
+        .find("h1")
+        .exists()
+    ).toBe(true);
   });
 });
